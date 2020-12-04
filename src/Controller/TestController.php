ec4ca55838215755fb7c39f9b348d3b78a7a6d2a
violaine <violainejacob80@gmail.com>
2020-12-04 12:22:58 +0100

    ajout controller et twig

---
+++ src/Controller/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/",name="index")
+     *  */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/test",name="test")
+     *  */
+    public function test()
+    {
+        $tab=[1,2,,3,];
+        return $this->render('test.html.twig',['tab'->$tab]);
+    }
+}
